503bb2c4c9fbceb3bfdd0fccf49e9ac0f76a7ad9
Neil Brennan <nello@anywhich.com>
2020-08-16 17:00:19 +1000

    Now with h-bridge for up to 6 coils

--- NPhaseSine/NPhaseSine.ino
+++ NPhaseSine/NPhaseSine.ino
@@ -4,18 +4,25 @@
 #include <vprintln.h>
 
 #define OUTPUT_PIN 12
-#define CHANNEL_COUNT 3
-#define RESOLUTION 120
+#define CHANNEL_COUNT 6
+#define RESOLUTION 30
+#define FQ 1
+
+#define H_BRIDGE true
 
 const float STEP = PI/RESOLUTION;
-float wave_offsets[CHANNEL_COUNT];
+float wave_offsets[CHANNEL_COUNT]; 
+int fq_delay = FQ > 0 ? (1000 / FQ / RESOLUTION) : 0;
 
 void setup() {
   delay(1000);
-//  Serial.begin(115200);
+  Serial.begin(115200);
 
-  for (int i=0; i < CHANNEL_COUNT; ++ i) {
+  for (int i=0; i < CHANNEL_COUNT * 2; ++ i) {
       pinMode(OUTPUT_PIN + i, OUTPUT);
+  }
+  pinMode(25, OUTPUT);
+  for (int i=0; i < CHANNEL_COUNT; ++ i) {
       wave_offsets[i] = i * TWO_PI / CHANNEL_COUNT;
   }
 }
@@ -24,11 +31,23 @@ void loop()  {
   for (float x = 0; x < TWO_PI; x = x + STEP) {
       //int sum = 0;
       for (int i=0; i < CHANNEL_COUNT; ++ i) {
-          int output = sin(x + wave_offsets[i]) * 128;
-          analogWrite(OUTPUT_PIN + i, output + 127);
+          int output = sin(x + wave_offsets[i]) * 256;   // better to build a wave table in setup()!!!
+   
+          if (H_BRIDGE) {
+            int output_channel = OUTPUT_PIN + (i << 1);
+            if (output > 0) {
+                analogWrite(output_channel == 20 ? 25 : output_channel, output);
+                analogWrite(output_channel + 1, 0);
+            } else {
+                analogWrite(output_channel == 20 ? 25 : output_channel, 0);
+                analogWrite(output_channel + 1, -output);
+            }
+          } else {
+            analogWrite(OUTPUT_PIN + i, output >> 1 + 127);
+          }
           //vprint(output, " "); sum = sum + output;
       }
       //vprintln("= ", sum);
-      //delay(1); 
+      if (fq_delay) delay(fq_delay); 
   }
 }
